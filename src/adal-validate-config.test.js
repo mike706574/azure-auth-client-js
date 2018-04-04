2a5a4e1ad0490b8f765a2f4f7439d096a8c39bf6
EAS7510 <michaeleaster@northwesternmutual.com>
2018-04-04 10:35:13 -0500

    Upgrade dev deps. Fix up config validation test.

--- src/adal-validate-config.test.js
+++ src/adal-validate-config.test.js
@@ -5,9 +5,9 @@ test('should not require domain', async () => {
                   clientId: "ae33c32e-d2f2-4992-a4b2-51d03e7c8677",
                   tenantId: "c834c34e-bbd3-4ea1-c2c2-51daeff91aa32"};
 
-  const valdiateConfig = validateConfig(config);
+  const validatedConfig = validateConfig(config);
 
-  expect(config).toEqual({
+  expect(validatedConfig).toEqual({
        "cacheLocation": "localStorage",
        "clientId": "ae33c32e-d2f2-4992-a4b2-51d03e7c8677",
        "clientType": "ADAL",
